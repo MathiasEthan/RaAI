4dfccd2ec6205a6d58b8a097287ca329900b4dbe
MathiasEthan <ethanmathias123@gmail.com>
2025-09-06 20:51:50 +0530

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { NavbarComponent } from "@/components/ui/NavbarComponent";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <NavbarComponent />
+        {children} 
+      </ThemeProvider>
       </body>
+     
+      
     </html>
   );
 }
